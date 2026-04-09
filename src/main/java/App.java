ff8fe8631d275d2d905dbbf52ac7c5c6a7e2bc43
Shanchal <shanchalsenthil@gmail.com>
2026-04-09 14:57:24 +0530

    Update greeting message in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Gradle Jenkins Pipeline");
+        System.out.println("Hello from Gradle Jenkins Scriptive Pipeline");
     }
 }
